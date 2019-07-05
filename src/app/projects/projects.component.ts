be0f4ff38eafa20908f26f56c3d3839d3af442d4
sanjog rijal <master@localhost.localdomain>
2019-07-05 14:24:58 +0545

    This is the new branch

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,15 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 projects = [
-  	["School Leaving Certificate","Budhanilkantha School","84%", "2003", "2010"],
-  	[ "A-Levels", "Budhanilkantha School/British Council",  "GPA: 3",  "2011",  "2012"],
-  	[ "Bachelors in Computer Information System", "Pokhara University",  "GPA: 3.5",  "2013",  "2017"],
-  	[ "Masters in Computer Application",  "Indira Gandhi National Open University",  "N/A",  "2018",  "Ongoing"]
+  	[ "Car Spa Management","Business Process Management","Krennova Pvt. Ltd.","Angular/Express Trainee", "2017", "2017","Internal System"],
+  	[ "Jobkhaani", "Job Portal/Web Application","Krennova Pvt. Ltd.",  "Full Stack Developer",  "2017",  "2018", "jobkhaani.com"],
+  	[ "Print-sewa", "E-Commerce","Krennova Pvt. Ltd.", "Full stack developer", "2017", "2018", "printsewa.com.np"],
+  	[ "Coffee MIS",  "Management Information System",  "Krennova Pvt. Ltd",  "Full Stack Developer",  "2018", "2018", "coffeemis.com"],
+    [ "Student Management System", "Management Information System", "SASOFT Enterprises Pvt. Ltd.", "Product Manager", "2018", "Ongoing", "sasoftsms.edumech.com.au" ]
   ];
 
-  colLength = [0,1,2,3,4];
-  tableHeaders = ['Name','University', 'Percentage', 'From', 'To'];
-  mainHeading = "Educational Details";
+  colLength = [0,1,2,3,4,5,6];
+  tableHeaders = ['Name','Project Type', 'Company Name', 'Role', 'From', 'To','URL'];
+  mainHeading = "Project Details";
   constructor() { }
 
   ngOnInit() {
